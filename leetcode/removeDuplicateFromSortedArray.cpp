91ba8c460406c2f5cff78dbc6dcf567cda56e313
asmit557 <162726938+asmit557@users.noreply.github.com>
2025-07-08 15:13:20 +0530

    removeDuplicateFromSortedArray.cpp

---
+++ leetcode/removeDuplicateFromSortedArray.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        set<int> s;
+        for(int i =0;i<nums.size();i++){
+              s.insert(nums[i]);
+        }
+        int index = 0; 
+        for(auto i:s){
+           nums[index] = i;
+           index++;
+        }
+        return s.size();
+    }
+};

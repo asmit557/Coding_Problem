fa4d87ba5a73ed72e39fc5d8b7b16b155aa5ec65
asmit557 <162726938+asmit557@users.noreply.github.com>
2025-08-06 10:46:30 +0530

    fruitsIntoBaskets.cpp

---
+++ leetcode/fruitsIntoBaskets.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int totalFruit(vector<int>& fruits) {
+        unordered_map<int,int> mp;
+        int n = fruits.size();
+
+        int i=0;
+        int j=0;
+        int maxi = 0;
+        while(j<n){
+            mp[fruits[j]]++;
+            while(mp.size()>2){
+                mp[fruits[i]]--;
+                if(mp[fruits[i]]==0){
+                    mp.erase(fruits[i]);
+                }
+                i++;
+            }
+            maxi = max(maxi,j-i+1);
+            j++;
+        }
+        return maxi;
+    }
+};

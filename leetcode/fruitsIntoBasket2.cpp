ce028e1caf76982b502c31bebcf5ef7b89c07ff9
asmit557 <162726938+asmit557@users.noreply.github.com>
2025-08-06 10:34:54 +0530

    fruitsIntoBasket2.cpp

---
+++ leetcode/fruitsIntoBasket2.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int numOfUnplacedFruits(vector<int>& fruits, vector<int>& baskets) {
+        int n = fruits.size();
+        int cnt = 0;
+        for(int i=0;i<n;i++){
+            bool b = false;
+           for(int j=0;j<n;j++){
+                if(baskets[j]>=fruits[i]){
+                    baskets[j] = 0;
+                    b = true;
+                     break;
+                }
+           }
+           if(b==false)cnt++;
+        }
+        return cnt;
+        
+    }
+};

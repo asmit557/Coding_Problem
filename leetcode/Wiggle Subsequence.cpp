377a9f50f9dd598ca7c4b1f0807a680a3621c91c
asmit557 <162726938+asmit557@users.noreply.github.com>
2025-08-10 13:30:38 +0530

    wiggleSubsequence.cpp

---
+++ leetcode/Wiggle Subsequence.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int wiggleMaxLength(vector<int>& nums) {
+        if (nums.size() < 2) return nums.size();
+        int prevDiff = nums[1] - nums[0];
+        int cnt = (prevDiff != 0) ? 2 : 1; 
+        for (int i = 2; i < nums.size(); i++) {
+            int diff = nums[i] - nums[i - 1];
+            if ((diff > 0 && prevDiff <= 0) || (diff < 0 && prevDiff >= 0)) {
+                cnt++;
+                prevDiff = diff;
+            }
+        }
+        return cnt;
+    }
+};
